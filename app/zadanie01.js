d6c4fd19b6b204dd1d1b1b5f682d29d1c7c7704d
Greg Herbowicz <herbowicz@users.noreply.github.com>
2018-02-02 11:49:18 +0100

    Update zadanie01.js

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,15 @@
-//Twój kod
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Podaj 2 liczby w URL');
+});
+
+app.get('/:first/:second', (req, res) => {
+  const sum = parseInt(req.params.first) + parseInt(req.params.second);
+  res.send('Suma ' + req.params.first + ' i ' + req.params.second + ' to: ' + sum);
+});
+
+app.listen(3000, () => {
+  console.log('Serwer uruchomiony na porcie 3000');
+});
