e24e0d533538e8ab3d72791d5a5e3a97000ea6a2
Maciej Woźniak <mwoz@softwebo.private>
2018-02-02 13:45:47 +0100

    fun with js #5

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,8 @@
-//Twój kod
+const express = require("express");
+const app = express();
+
+app.get("/:number1/:number2", (req, res) => {
+    res.send( (parseInt(req.params.number1)+parseInt(req.params.number2)).toString() );
+});
+
+app.listen(3000);
--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,34 @@
-//Twój kod
+const express = require("express");
+const app = express();
+
+app.use(express.static('./public/zadanieDnia/'));
+
+let answers = [];
+
+app.get("/vote/:answer", (req, resp) => {
+    const answer = req.params.answer;
+    if (typeof answers[answer] === 'undefined') {
+        answers[answer] = 1;
+    }
+    else {
+        answers[answer]++;
+    }
+
+    resp.send("Dziekujemy za głos na " + answer + ". Aktualnie posiada on: " + answers[answer] + " głosów.");
+});
+
+app.get("/votes/check", (req, resp) => {
+    let answer = "Wyniki ankiety: <ul>";
+
+    for (let index in answers) {
+        if (answers.hasOwnProperty(index)) {
+            answer += "<li>" + index + " - głosów " + answers[index] + "</li>"
+        }
+    }
+
+    answer += "</ul>";
+
+    resp.send(answer);
+});
+
+app.listen(3000);
