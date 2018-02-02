f33c9e5848cd8dedb71355e1fb98db4a4da05d94
Greg Herbowicz <herbowicz@users.noreply.github.com>
2018-02-02 12:13:16 +0100

    Update zadanie02.js

--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,27 @@
-//Twój kod
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('');
+});
+
+let names = [];
+
+app.get('/name/set/:imie', (req, res) => {
+  names.push(req.params.imie);
+  res.send(`Imię ${req.params.imie} dodane.`);
+});
+
+app.get('/name/show', (req, res) => {
+  res.send('Aktualne imię to: ' + names[names.length-1]);
+});
+
+app.get('/name/check/:imie', (req, res) => {  //poprawiona ścieżka
+  names.indexOf(req.params.imie) < 0 ?
+    res.send(`Imię ${req.params.imie} nie zostało jeszcze zapisane.`) :
+    res.send(`Imię ${req.params.imie} zostało już zapisane.`);
+});
+
+app.listen(3000, () => {
+  console.log('Serwer uruchomiony na porcie 3000');
+});
