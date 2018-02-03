ef129bc0ba755fe2e2e09ad15671ea97dc8ae373
Jakub Matwiejew <custom@audiocustom.pl>
2018-02-03 23:53:46 +0100

    Dzień 5

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,17 @@
 //Twój kod
+
+const express = require('express');
+const app = express();
+
+app.get('/:firstnumber/:secondnumber', (req, res) => {
+
+    const firstnumber = parseInt(req.params.firstnumber);
+    const secondnumber = parseInt(req.params.secondnumber);
+    let result = firstnumber + secondnumber;
+
+    res.send('Wynik to: ' + result);
+});
+
+app.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,28 @@
 //Twój kod
+
+const express = require('express');
+const app = express();
+
+let name = '';
+
+app.get('/name/set/:imie', (req, res) => {
+    name = req.params.imie;
+    res.send('Zapamiętałem imię: ' + name);
+});
+
+app.get('/name/show', (req, res) => {
+    res.send('Zapamiętane imię to: ' + name);
+});
+
+app.get('/name/check', (req, res) => {
+    if(name.length > 0){
+        res.send('Imię ' + name + ' zostało już zapamiętane!');
+    } else {
+        res.send('Żadne imię nie zostało zapamiętane!');
+    }
+});
+
+
+app.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
