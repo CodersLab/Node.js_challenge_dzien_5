8dc8d28555745e2fed2c31b2206024e51e88f894
Maciej Judka <maciej.judka@gmail.com>
2018-02-03 08:21:43 +0100

    Add zadanie01

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,10 @@
-//Twój kod
+const express = require('express');
+const app = express();
+
+app.get('/sum/:num1/:num2', (req, res) => {
+	res.send(`Wynik: ${Number(req.params.num1)+Number(req.params.num2)}`);
+});
+
+app.listen(3000, () => {
+	console.log('Serwer uruchomiony na porcie 3000');
+});
