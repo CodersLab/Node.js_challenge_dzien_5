4729bc72f4d0ea3816a9b6ceef9a84b3460ce4de
RadekM86 <byrm.design@gmail.com>
2018-02-03 11:11:44 +0100

    Zadanie 1 i 2

--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,28 @@
 //Twój kod
+const express = require('express');
+const app = express();
+
+var name =""
+app.use(express.static('/'));
+
+app.get('/name/set/:imie', (req, res) => { 
+    name = req.params.imie;
+    res.send('Witaj, ' + name);
+});
+app.get('/name/show', (req, res) => { 
+    res.send('Imię: ' + name);
+});
+
+
+app.get('/name/check', (req, res) => { 
+    if(name.length>0){
+    res.send('Imię: ' + name)
+    }else{
+        res.send("nie podano imienia")
+    }
+});
+
+
+app.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});``
