ab096a2fc42ffc6149f7b0c782dca74fd17b7a96
Markowiecky <markowiecki.z@gmail.com>
2018-02-04 18:48:27 +0100

    Uzupełnione zadania - dzień 5

--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,22 @@
-//Twój kod
+const express = require('express');
+const app = express();
+let name = '';
+app.get('/name/set/:name', (req,res) =>{
+  name = req.params.name;
+  res.send('Podane imię to: '+name);
+});
+app.get('/name/show', (req,res) =>{
+  res.send('Podane imię to: '+name);
+});
+app.get('/name/check', (req, res) =>{
+  let text = '';
+    if(name.length>0){
+    text = 'To jest w Check, i imie to:  '+name;
+  } else{
+    text = 'Imię nie zostało podane!';
+  }
+  res.send(text);
+});
+app.listen(3000, () =>{
+  console.log('Uruchomiono serwer na porcie 3000');
+})
