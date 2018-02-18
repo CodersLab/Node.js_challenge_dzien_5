92ce3a933a569c6c77e4c8da0cb263254a2de9b5
Anna Górzanowska <anna.gorzanowska@gmail.com>
2018-02-18 10:58:10 +0100

    Ex. 02 done

--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,20 @@
-//Twój kod
+const express = require('express');
+const app = express();
+let userName = '';
+
+app.get('/name/set/:imie', (req, res) => {
+    userName = req.params.imie;
+res.send('Twoje imię to ' + userName);
+});
+
+app.get('/name/show', (req, res) => {
+    res.send('Zapamiętałem Twoje imię: ' + userName);
+});
+
+app.get('/name/check', (req, res) => {
+    (userName !=='')? (res.send('Zapamiętałem Twoje imię: ' + userName)) : (res.send('Nie znam Twojego imienia :('));
+});
+
+app.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
