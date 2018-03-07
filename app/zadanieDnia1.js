830db084d6308f0b5636c7dd2c1f08baa1248642
Anna Górzanowska <anna.gorzanowska@gmail.com>
2018-03-07 09:39:38 +0100

    the solution with variables

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -5,26 +5,36 @@ let noCounter = 0;
 let maybeCounter = 0;
 app.use(express.static('./public/zadanieDnia/'));
 let pollCounter = ()=> {
-    app.get('/vote/yes', (req, res) => {
-        yesCounter++;
+    // app.get('/vote/yes', (req, res) => {
+    //     // yesCounter++;
+    //     res.send('Dziękujemy za głos!');
+    // });
+    // app.get('/vote/no', (req, res) => {
+    //     // noCounter++;
+    //     res.send('Dziękujemy za głos!');
+    // });
+    // app.get('/vote/maybe', (req, res) => {
+    //     // maybeCounter++;
+    //     res.send('Dziękujemy za głos!');
+    // });
+    app.get('/vote/:votetype', (req, res) => {
+        let votetype = req.params.votetype;
+        if (votetype === 'yes') {
+            yesCounter++;
+        }
+        else if (votetype === 'no'){
+            noCounter++;
+        }
+        else {
+            maybeCounter++;
+        }
         res.send('Dziękujemy za głos!');
     });
-    app.get('/vote/no', (req, res) => {
-        noCounter++;
-        res.send('Dziękujemy za głos!');
-    });
-    app.get('/vote/maybe', (req, res) => {
-        maybeCounter++;
-        res.send('Dziękujemy za głos!');
-    });
-
     app.get('/votes/check', (req, res) => {
         res.send(`'Tak': ${yesCounter} 'Nie': ${noCounter} 'To się okaże': ${maybeCounter}`);
     });
 };
 pollCounter();
-
-
 app.listen(3000, () => {
     console.log('Serwer uruchomiony na porcie 3000');
 });
