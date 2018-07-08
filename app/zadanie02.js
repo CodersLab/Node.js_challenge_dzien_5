8160b9827b2061e14ab180ab97cd9343d530f1f6
Magdalena Goralczyk <magda.g10@wp.pl>
2018-07-08 14:18:31 +0200

    zadanie02

--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,8 +1,27 @@
 const express = require('express');
 
 const app = express();//Twój kod
-app.get('/hello/:myname', (req, res) => { //np. localhost:3000/hello/Programist(k)o
-    const name = req.params.myname;
-    res.send('Witaj, ' + name);
-    
+var imie = null;
+
+app.get('/name/set/:imie', (req, res) => { //np. localhost:3000/hello/Programist(k)o
+    imie = req.params.imie;
+    //res.send('Witaj, ' + imie);
+
+});
+
+app.get('/name/show', (req, res) => { //np. localhost:3000/hello/Programist(k)o
+    res.send('Witaj, ' + imie);
+
+});
+app.get('/name/check', (req, res) => { //np. localhost:3000/hello/Programist(k)o
+    if (imie==null){
+    res.send('Imie nie bylo podane ');
+}
+    else  {
+        res.send('Imie zostalo podane');
+    }
+
+});
+app.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
 });
