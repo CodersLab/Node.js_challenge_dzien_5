4a2b743af37b2751b411c0cb5ad27e27c7aad9ad
Magdalena Goralczyk <magda.g10@wp.pl>
2018-07-08 13:56:07 +0200

    zadanie01

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,17 @@
-//Twój kod
+const express = require('express');
+
+const app = express();
+
+app.get('/:var1/:var2', (req, res) => { //np. localhost:3000
+    const var1 = req.params.var1;
+    const var2 = req.params.var2;
+    const intvar1 = parseInt(var1);
+    const intvar2 = parseInt(var2);
+    const sum=intvar1 + intvar2;
+    
+    res.send(String(sum));
+});
+
+app.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,8 @@
-//Twój kod
+const express = require('express');
+
+const app = express();//Twój kod
+app.get('/hello/:myname', (req, res) => { //np. localhost:3000/hello/Programist(k)o
+    const name = req.params.myname;
+    res.send('Witaj, ' + name);
+    
+});
