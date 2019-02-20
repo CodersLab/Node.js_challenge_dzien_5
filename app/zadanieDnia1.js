b751ba0e7bd90cb5004440f1fc633044cf52410e
Your Name <you@example.com>
2019-02-20 19:38:25 +0100

    Day 5 completed

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,27 @@
 //Twój kod
+const express = require('express');
+const app = express();
+let countYes = 0;
+let countNo = 0;
+app.use(express.static('./public/zadanieDnia/'));
+
+app.get('/vote/yes', (req, res) => {
+    countYes += 1;
+    res.send(`yes <br>
+    <a href="/">Wróć</a>`)
+})
+
+app.get('/vote/no', (req, res) => {
+    countNo += 1;
+    res.send(`no <br>
+    <a href="/">Wróć</a>`)
+})
+
+app.get('/votes/check', (req, res) => {
+    res.send(`Node.js lubią ${countYes}, nie lubią ${countNo} osoby <br>
+    <a href="/">Wróć</a>`)
+})
+
+app.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie http://localhost:3000/')
+})
